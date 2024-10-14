c58664d3260062297d4c066783e14029bed4afa1
DatAKAjezz <jezzthedead@gmail.com>
2024-10-14 17:35:27 +0700

    .

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb emplace_back
+#define F first
+#define S second
+#define fora(x, a) for(auto &x : a)
+
+void solve(){
+
+    
+
+}
+
+int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen(".inp", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif 
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // int t; cin >> t;while(t--) 
+    solve();
+
+}
